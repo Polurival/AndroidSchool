c3e2f32437f103c0e1fa111b5839081623ab21dc
SIGMA\SBT-Polshchikov-YuV <SBT-Polshchikov-YuV@sberbank.ru>
2017-06-13 16:35:47 +0300

    added swipeToRefresh all cities loading

--- LoaderWeather/app/src/main/java/ru/gdgkazan/simpleweather/screen/weatherlist/WeatherListActivity.java
+++ LoaderWeather/app/src/main/java/ru/gdgkazan/simpleweather/screen/weatherlist/WeatherListActivity.java
@@ -3,10 +3,14 @@ package ru.gdgkazan.simpleweather.screen.weatherlist;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -19,12 +23,15 @@ import ru.gdgkazan.simpleweather.model.City;
 import ru.gdgkazan.simpleweather.screen.general.LoadingDialog;
 import ru.gdgkazan.simpleweather.screen.general.LoadingView;
 import ru.gdgkazan.simpleweather.screen.general.SimpleDividerItemDecoration;
+import ru.gdgkazan.simpleweather.screen.weather.RetrofitWeatherLoader;
 import ru.gdgkazan.simpleweather.screen.weather.WeatherActivity;
 
 /**
  * @author Artur Vasilov
  */
 public class WeatherListActivity extends AppCompatActivity implements CitiesAdapter.OnItemClick {
+
+    private static final String TAG = WeatherListActivity.class.getSimpleName();
 
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
@@ -35,9 +42,14 @@ public class WeatherListActivity extends AppCompatActivity implements CitiesAdap
     @BindView(R.id.empty)
     View mEmptyView;
 
+    @BindView(R.id.swipeRefresh)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
     private CitiesAdapter mAdapter;
 
     private LoadingView mLoadingView;
+
+    private String mCityName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -51,6 +63,11 @@ public class WeatherListActivity extends AppCompatActivity implements CitiesAdap
         mAdapter = new CitiesAdapter(getInitialCities(), this);
         mRecyclerView.setAdapter(mAdapter);
         mLoadingView = LoadingDialog.view(getSupportFragmentManager());
+
+        mSwipeRefreshLayout.setOnRefreshListener(() -> {
+            mSwipeRefreshLayout.setRefreshing(false);
+            loadWeather(true);
+        });
 
         /**
          * TODO : task
@@ -67,6 +84,7 @@ public class WeatherListActivity extends AppCompatActivity implements CitiesAdap
          *
          * But you should think how to manage this case. I suggest you to start from reading docs mindfully.
          */
+        loadWeather(false);
     }
 
     @Override
@@ -82,5 +100,55 @@ public class WeatherListActivity extends AppCompatActivity implements CitiesAdap
             cities.add(new City(city));
         }
         return cities;
+    }
+
+    private void loadWeather(boolean restart) {
+        mLoadingView.showLoadingIndicator();
+        LoaderManager.LoaderCallbacks<City> callbacks = new WeatherListActivity.WeatherCallbacks();
+        for (int i = 0; i < getInitialCities().size(); i++) {
+            mCityName = getInitialCities().get(i).getName();
+            if (restart) {
+                getSupportLoaderManager().restartLoader(R.id.weather_loader_id + i, Bundle.EMPTY, callbacks);
+            } else {
+                getSupportLoaderManager().initLoader(R.id.weather_loader_id + i, Bundle.EMPTY, callbacks);
+            }
+        }
+    }
+
+    private void showWeather(@Nullable City city) {
+        if (city == null || city.getMain() == null || city.getWeather() == null
+                || city.getWind() == null) {
+            showError();
+            return;
+        }
+        Log.d(TAG, "City " + city.getName() + ", temperature " + city.getMain().getTemp());
+        mLoadingView.hideLoadingIndicator();
+    }
+
+    private void showError() {
+        Log.d(TAG, "loading error for " + mCityName);
+        mLoadingView.hideLoadingIndicator();
+    }
+
+    private class WeatherCallbacks implements LoaderManager.LoaderCallbacks<City> {
+
+        @Override
+        public Loader<City> onCreateLoader(int id, Bundle args) {
+            Log.d(TAG, "onCreateLoader " + id);
+            return new RetrofitWeatherLoader(WeatherListActivity.this, mCityName);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<City> loader, City city) {
+            Log.d(TAG, "onLoadFinished " + loader.getId());
+            showWeather(city);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<City> loader) {
+            Log.d(TAG, "onLoaderReset " + loader.getId());
+            mCityName = null;
+            // Do nothing
+        }
     }
 }
